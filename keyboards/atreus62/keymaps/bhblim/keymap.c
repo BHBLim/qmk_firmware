406669e626fb2cbf5b1d6d67c854e06a82fa9e8a
Basil Lim <BHB_Lim@yahoo.com>
2022-09-13 21:57:04 +0800

    Modified Atreus62 keymap

--- keyboards/atreus62/keymaps/bhblim/keymap.c
+++ keyboards/atreus62/keymaps/bhblim/keymap.c
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
  * Numpad
  * ,-----------------------------------------.                   ,-----------------------------------------.
- * |      |      |      |      |  F11 |  F12 |                   |Numlok|      |      |      |      |  del |
+ * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |                   |Numlok|      |      |      |      |  del |
  * |------+------+------+------+------+------|                   |------+------+------+------+------+------|
  * | Tab  | Vol+ | Home | Up   |  End | PgUp |                   |  -   |   7  |   8  |   9  |   *  | Bspc |
  * |------+------+------+------+------+------|                   |------+------+------+------+------+------|
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  `----------------------------------------------         '------'z'--------------------------------------'
  */
 	[6] = LAYOUT(
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_F11, KC_F12,                                                 KC_NLCK, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                                KC_NLCK, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
         KC_TRNS, KC_VOLU, KC_HOME, KC_UP, KC_END, KC_PGUP,                                          KC_PMNS, KC_P7, KC_P8, KC_P9, KC_PAST, KC_TRNS,
         KC_TRNS, KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                                       KC_PPLS, KC_P4, KC_P5, KC_P6, KC_PSLS, KC_NO,
         KC_TRNS, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,                                         KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_PENT,
